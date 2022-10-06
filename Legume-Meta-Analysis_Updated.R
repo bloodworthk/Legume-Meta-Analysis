1883c7977529a037d3db9928e6a253bdfd719cd4
Bloodworthk <bloodworthk@si.edu>
2022-10-06 10:42:11 -0400

    update notes

--- Legume-Meta-Analysis_Updated.R
+++ Legume-Meta-Analysis_Updated.R
@@ -20,8 +20,7 @@ library(metagear)
 #read in data frame with the Web of Science search results from Oct. 2022
 WoS_Abstracts_2022<-read.csv("Methods/Web_of_Science_10_05_Search_Results.csv")
 
-#### Setting up Screening Tasks ####
-#DO NOT RUN THIS
+#### Setting up Screening Tasks - DO NOT RUN ANY OF THIS AGAIN ####
 
 #This step adds 4 new columns (Study ID, Reviewer, 2 columns for include/or dont include)
 References_Screening_Ready<-effort_initialize(WoS_Abstracts_2022)
@@ -32,7 +31,7 @@ theTeam<-c("Kathryn","Kim","Smriti")
 #Randomly delegate screening efforts between the team of reviewers evenly and then save these as seperate files to the directory given
 References_unscreened<-effort_distribute(References_Screening_Ready,reviewers=theTeam, save_split = TRUE,directory = "C:/Users/kjbloodw/OneDrive - UNCG/Invasive Legume Meta-Analysis/Methods/2022_Reference_Screening")
 
-#### Screen Abstracts ####
+#### Screen Abstracts - START HERE ####
 #KIM AND SMRITI START HERE: Run only the line that applies to you (has your csv file in it)
 
 abstract_screener(file=file.choose("C:/Users/kjbloodw/OneDrive - UNCG/Invasive Legume Meta-Analysis/Methods/2022_Reference_Screening/effort_Kathryn.csv"),
