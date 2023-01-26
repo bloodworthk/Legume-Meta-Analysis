05e1f243df32b82d2b9e41b28e73b0eba1eb29c5
bloodworthk <kathryn.bloodworth3@gmail.com>
2023-01-26 13:03:51 -0500

    look at strain richness

--- Legume_Meta_Analysis_Official.R
+++ Legume_Meta_Analysis_Official.R
@@ -6,12 +6,8 @@
 
 #install.packages("ggplot2")
 library(ggplot2)
-#install.packages("lme4")
-library(lme4)
 #install.packages("lmerTest")
 library(lmerTest)
-#install.packages("nlme")
-library(nlme)
 #install.packages("tidyverse")
 library(tidyverse)
 
@@ -179,3 +175,162 @@ anova(Mixed_Model_Rhiz_Status,type = 2)
 summary(Mixed_Model_Rhiz_Status_NoMean_Random<- lmer(diversity_sum ~ Plant_status + (1 | genus_species) + (1 | num_nodules), data = Rhizobial_Associations_Condensed)) 
 #anova accounting for type II error - for unbalanced data or interaction terms that are significant 
 anova(Mixed_Model_Rhiz_Status_NoMean_Random,type = 2) 
+
+#### Rhizobial Associates by Growth Form  ####
+
+Plant_Data_simplified<-Plant_Data %>% 
+  select(paper_id,author,year,genus_species,annual_perennial,growth_form)
+
+#Make a new data frame called rhizobial_associates_growth_form
+Rhizobial_Associations_growth_form<-Plant_Associations%>%
+  mutate(Plant_status=ifelse(plant_status=="introduced","non_native",ifelse(plant_status=="invasive","non_native",ifelse(plant_status=="native","native",ifelse(plant_status=="extinct","extinct",ifelse(plant_status=="introduced-native","non_native",ifelse(plant_status==" native","native",plant_status))))))) %>% 
+  left_join(Plant_Data_simplified)
+
+#Sum across all columns with presence absence data for each row, giving us the number of assiciations for each species from each paper
+Rhizobial_Associations_growth_form$diversity_sum <- as.numeric(apply(Rhizobial_Associations_growth_form[,27:312], 1, sum))
+
+#Make a new condensed dataframe with only necessary information
+Rhizobial_Associations_GF_Condensed<-Rhizobial_Associations_growth_form %>% 
+  select(paper_id, genus_species, sample_country,diversity_sum,annual_perennial,growth_form,num_nodules,Plant_status) %>% 
+  na.omit() %>% 
+  filter(Plant_status %in% c("native","non_native"))
+
+#Make a data frame calculating the mean, standard error, and number of observations for native vs. non-native to make graph
+Average_growth_form_Graph<-Rhizobial_Associations_GF_Condensed%>%
+  group_by(growth_form)%>%
+  summarize(symbionts_Std=sd(diversity_sum),symbionts_Mean=mean(diversity_sum),symbionts_n=length(diversity_sum))%>%
+  #Make a new column called "Richness_St_Error" and divide "Richness_Std" by the square root of "Richness_n"
+  mutate(symbionts_St_Error=symbionts_Std/sqrt(symbionts_n))
+
+#Make graph showing number of introduced vs. native
+ggplot(Average_growth_form_Graph,aes(x=growth_form,y=symbionts_Mean,fill=growth_form))+
+  #Make a bar graph where the height of the bars is equal to the data (stat=identity) and you preserve the vertical position while adjusting the horizontal(position_dodge), and outline the bars with the color black.
+  geom_bar(stat="identity", position=position_dodge(),color="black")+
+  geom_errorbar(aes(ymin=symbionts_Mean-symbionts_St_Error,ymax=symbionts_Mean+symbionts_St_Error),position=position_dodge(0.9),width=0.2)+
+  #make error bars using the Standard error from the mean and place them at 0.9 with a width of 0.2
+  #Label the x-axis "Watershed"
+  xlab("Growth Form")+
+  #Label the y-axis "Species Richness"
+  ylab("Average Number of Symbionts")#+
+  #scale_fill_manual(values=c("darkslategrey","cadetblue4","cadetblue3"))  
+
+
+####Anova for growth form ####
+summary(Mixed_Model_Rhiz_Status_Growth_Form<- lmer(diversity_sum ~ growth_form + (1|num_nodules), data = Rhizobial_Associations_GF_Condensed)) #
+anova(Mixed_Model_Rhiz_Status_Growth_Form,type=2) 
+
+#t.test(Rhizobial_Associations_GF_Condensed$diversity_sum~Rhizobial_Associations_GF_Condensed$growth_form)
+
+#### Rhizobial Associates by Annual/Perennial  ####
+
+#Make a data frame calculating the mean, standard error, and number of observations for native vs. non-native to make graph
+Average_annual_perennial_Graph<-Rhizobial_Associations_GF_Condensed%>%
+  group_by(annual_perennial,Plant_status)%>%
+  summarize(symbionts_Std=sd(diversity_sum),symbionts_Mean=mean(diversity_sum),symbionts_n=length(diversity_sum))%>%
+  #Make a new column called "Richness_St_Error" and divide "Richness_Std" by the square root of "Richness_n"
+  mutate(symbionts_St_Error=symbionts_Std/sqrt(symbionts_n))
+
+#Make graph showing number of introduced vs. native and perennial / annual
+ggplot(Average_annual_perennial_Graph,aes(x=annual_perennial,y=symbionts_Mean,fill=Plant_status))+
+  #Make a bar graph where the height of the bars is equal to the data (stat=identity) and you preserve the vertical position while adjusting the horizontal(position_dodge), and outline the bars with the color black.
+  geom_bar(stat="identity", position=position_dodge(),color="black")+
+  geom_errorbar(aes(ymin=symbionts_Mean-symbionts_St_Error,ymax=symbionts_Mean+symbionts_St_Error),position=position_dodge(0.9),width=0.2)+
+  #make error bars using the Standard error from the mean and place them at 0.9 with a width of 0.2
+  #Label the x-axis "Watershed"
+  xlab("Growth Timing")+
+  #Label the y-axis "Species Richness"
+  ylab("Average Number of Symbionts")
+#scale_fill_manual(values=c("darkslategrey","cadetblue4"))  
+
+#Make a data frame calculating the mean, standard error, and number of observations for native vs. non-native to make graph
+Average_status_herb_Graph<-Rhizobial_Associations_GF_Condensed %>%
+  mutate(Growth_Status=paste(growth_form,annual_perennial,sep = "::")) %>% 
+  group_by(Growth_Status,Plant_status) %>%
+  summarize(symbionts_Std=sd(diversity_sum),symbionts_Mean=mean(diversity_sum),symbionts_n=length(diversity_sum)) %>%
+  #Make a new column called "Richness_St_Error" and divide "Richness_Std" by the square root of "Richness_n"
+  mutate(symbionts_St_Error=symbionts_Std/sqrt(symbionts_n))
+
+#Make graph showing number of introduced vs. native and herb/woody and annual/perennial
+ggplot(subset(Average_status_herb_Graph,Growth_Status!="woody::annual"),aes(x=Growth_Status,y=symbionts_Mean,fill=Plant_status))+
+  #Make a bar graph where the height of the bars is equal to the data (stat=identity) and you preserve the vertical position while adjusting the horizontal(position_dodge), and outline the bars with the color black.
+  geom_bar(stat="identity", position=position_dodge(),color="black")+
+  geom_errorbar(aes(ymin=symbionts_Mean-symbionts_St_Error,ymax=symbionts_Mean+symbionts_St_Error),position=position_dodge(0.9),width=0.2)+
+  #make error bars using the Standard error from the mean and place them at 0.9 with a width of 0.2
+  #Label the x-axis "Watershed"
+  xlab("Growth Timing")+
+  #Label the y-axis "Species Richness"
+  ylab("Average Number of Symbionts")#+
+#scale_fill_manual(values=c("darkslategrey","cadetblue4"))  
+
+####Anova for annual/perennial ####
+summary(Mixed_Model_Rhiz_Status_A_P<- lmer(diversity_sum ~ annual_perennial + (1|num_nodules), data = Rhizobial_Associations_GF_Condensed)) #
+anova(Mixed_Model_Rhiz_Status_A_P,type=2) 
+
+#t.test(Rhizobial_Associations_GF_Condensed$diversity_sum~Rhizobial_Associations_GF_Condensed$annual_perennial) 
+
+####Anova for annual/perennial*native/nonnative ####
+summary(Mixed_Model_Rhiz_Status_A_P_Plant_status<- lmer(diversity_sum ~ annual_perennial*Plant_status + (1 | genus_species) + (1|num_nodules), data = Rhizobial_Associations_GF_Condensed))
+#type 3 is used because we have an interaction
+anova(Mixed_Model_Rhiz_Status_A_P_Plant_status,type = 3) 
+
+####Anova for annual/perennial*native/nonnative*growth form ####
+summary(Mixed_Model_Rhiz_Status_A_P_Status_GF<- lmer(diversity_sum ~ annual_perennial*Plant_status + growth_form*Plant_status + (1 | genus_species) + (1|num_nodules), data = Rhizobial_Associations_GF_Condensed)) #
+anova(Mixed_Model_Rhiz_Status_A_P_Status_GF,type = 3) 
+
+#### Number Nodules (sample number) by strain richness ####
+
+Nod_by_strain<-Plant_Data %>%
+  filter(strain_richness!="NEED SUPPLEMENTAL PAPER")%>%
+  filter(!is.na(num_nodules),!is.na(strain_richness))%>% 
+  mutate(strain_richness=as.numeric(strain_richness)) %>% 
+  filter(num_nodules>10) %>% 
+  group_by(genus_species) %>% 
+  summarize(num_nod_Mean=mean(num_nodules),num_nod_n=length(num_nodules),strain_rich_Mean=mean(strain_richness),strain_rich_n=length(strain_richness))%>%
+  ungroup()
+
+hist(Nod_by_strain$num_nod_Mean) 
+
+#graph looking at strain richness by nodule number
+ggplot(Nod_by_strain,aes(y=strain_rich_Mean,x=num_nod_Mean))+
+  geom_point()+
+  geom_smooth()+
+  #keep entire graph but zoom in on certain area
+  coord_cartesian(xlim=c(0,100))
+
+#merge plant status with number of nodules
+Rhiz_symbionts_Nod_Num<-Nod_by_strain %>% 
+  left_join(Average_rhizobial_symbionts_genus_sp) %>% 
+  select(-symbionts_Std,-symbionts_St_Error) %>% 
+  filter(!is.na(num_nod_Mean),!is.na(Plant_status))
+
+#look at difference between strain richness number and symbionts number
+ggplot(Rhiz_symbionts_Nod_Num,aes(y=strain_rich_Mean,x=symbionts_Mean))+
+  geom_point()+
+  geom_abline()
+
+#make dataframe for graph
+Strain_rich_graph<-Rhiz_symbionts_Nod_Num %>% 
+  group_by(Plant_status) %>% 
+  summarize(strain_rich_Mean2=mean(strain_rich_Mean),strain_rich_n2=length(strain_rich_Mean),strain_rich_Std=sd(strain_rich_Mean))%>%
+  mutate(strain_rich_St_Error=strain_rich_Std/sqrt(strain_rich_n2)) %>% 
+  ungroup()
+
+#Make graph showing average strain richness by introduced vs. native
+ggplot(Strain_rich_graph,aes(x=Plant_status,y=strain_rich_Mean2,fill=Plant_status))+
+  #Make a bar graph where the height of the bars is equal to the data (stat=identity) and you preserve the vertical position while adjusting the horizontal(position_dodge), and outline the bars with the color black.
+  geom_bar(stat="identity", position=position_dodge(),color="black")+
+  geom_errorbar(aes(ymin=strain_rich_Mean2-strain_rich_St_Error,ymax=strain_rich_Mean2+strain_rich_St_Error),position=position_dodge(0.9),width=0.2)+
+  #make error bars using the Standard error from the mean and place them at 0.9 with a width of 0.2
+  #Label the x-axis "Watershed"
+  xlab("Plant Species Status")+
+  #Label the y-axis "Species Richness"
+  ylab("Average Strain Richness")#+
+  #scale_fill_manual(values=c("darkslategrey","cadetblue4"), labels=c("Native","Non-native"))  
+
+hist(Rhizobial_Associations_Condensed$diversity_sum)
+
+#Run anova accounting for how many species and how many nodules they sample
+#### run this again without mean and accounting for nodule number #### did this above
+summary (Mixed_Model_Rhiz_Status_Strain_rich<- lmer(strain_rich_Mean ~ Plant_status + (1 | species_status_n) + (1 | num_nod_Mean), data = Rhiz_symbionts_Nod_Num))
+anova(Mixed_Model_Rhiz_Status_Strain_rich,type = 2)
+
