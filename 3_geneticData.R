42fa0d2c3c9f59689917c4c190a46309ec7462fc
klapierre <kjkomatsu@uncg.edu>
2023-01-27 16:19:40 -0500

    Adding in any gene region across all spp.

--- 3_geneticData.R
+++ 3_geneticData.R
@@ -42,21 +42,30 @@ allSpp <- plantData %>%
   filter(!is.na(genus_species)) %>% 
   unique() #790 legume species examined (including native/introduced spp and some duplicates for spp that were sampled both at home and away)
 
+numSpp <- plantData %>% 
+  select(genus_species) %>% 
+  filter(!is.na(genus_species)) %>% 
+  unique() #711 legume species examined (including native/introduced spp and some duplicates for spp that were sampled both at home and away)
+
 introducedSpp <- allSpp %>% 
   filter(plant_status=='introduced') #103 introduced legume spp
 
+anyGenetic <- plantData %>% 
+  filter(genbank==1) %>% #2261 observations of those that have genetic data
+  select(genus_species, plant_status, genbank, genetic_region) %>% 
+  unique() %>% 
+  pivot_wider(names_from=genetic_region, values_from=genbank, values_fill=NA) %>% 
+  select(-genus_species) %>% 
+  group_by(plant_status) %>% 
+  summarise_all(funs(sum), na.rm=T) %>% 
+  ungroup() #we have a lot of good gene regions that we can use for comparisons across all native vs introduced spp, many fewer for specific comparisons across individual spp in native and introduced ranges
 
 
-
-
-#Read in strain diversity info and add NA for anywhere that has a blank
-strainDiversity <- read.csv("Data/legume_strain diversity_meta analysis_strain sequences.csv") %>%
-  #change all 999 to NAs
-  mutate_all(~na_if(., 999))
-strainDiversity[strainDiversity==""]<-NA
-strainDiversity <- strainDiversity %>% 
-  select(paper_id, genus, species, plant_status, sample_country, sample_continent, genetic_region, strain, accession_number) %>% 
-  left_join(plantData)
-
-homeAway <- strainDiversity %>% 
-  filter(compares_homeaway==1)
+# #Read in strain diversity info and add NA for anywhere that has a blank
+# strainDiversity <- read.csv("Data/legume_strain diversity_meta analysis_strain sequences.csv") %>%
+#   #change all 999 to NAs
+#   mutate_all(~na_if(., 999))
+# strainDiversity[strainDiversity==""]<-NA
+# strainDiversity <- strainDiversity %>% 
+#   select(paper_id, genus, species, plant_status, sample_country, sample_continent, genetic_region, strain, accession_number) %>% 
+#   left_join(plantData)
