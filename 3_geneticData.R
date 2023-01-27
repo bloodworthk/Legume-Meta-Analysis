ef66ce5272a1fcf41a1be267ced1411eb9e8691e
klapierre <kjkomatsu@uncg.edu>
2023-01-27 12:37:26 -0500

    Script set up.

---
+++ 3_geneticData.R
@@ -0,0 +1,7 @@
+#### Legume Meta Analysis ####
+#Authors: Kathryn Bloodworth, Smriti Pehim Limbu, Kim Komatsu
+
+setwd('G:\\.shortcut-targets-by-id\\1w2OXIzBKQqFZ0BCeKP7C9pX36ViGDPBj\\Legume-Meta Analysis') #kim's wd
+
+library(tidyverse)
+
