d60bb3e46c96e19afa62292551d821ab5359aa0a
Kimberly Komatsu <KJKOMATSU@uncg.edu>
2023-02-24 13:18:53 -0500

    Merging with cultivation status.

--- 2_cleaning_species_names.R
+++ 2_cleaning_species_names.R
@@ -22,24 +22,31 @@ setwd('G:\\.shortcut-targets-by-id\\1w2OXIzBKQqFZ0BCeKP7C9pX36ViGDPBj\\Legume-Me
 #### import data ####
 
 full.list <- read.csv ("legume_old_names.csv") %>% 
+  select(-genus_species) %>% 
+  unite(col=genus_species, c(genus, species), sep=' ')
+
+genus.species <- full.list %>% 
   select(genus, species) %>% 
   unite(col=genus_species, c(genus, species), sep=' ')
 
 
 #### finding recognized names ####
 
-TPL.output <- TPL(full.list$genus_species)
+TPL.output <- TPL(genus.species$genus_species)
 
 clean.list <- TPL.output %>% 
-  rename(old_name=Taxon) %>% 
+  rename(genus_species=Taxon) %>% 
   unite(col=new_name, c(New.Genus, New.Species), sep=' ') %>% 
-  select(Plant.Name.Index, old_name, new_name) %>% 
+  select(Plant.Name.Index, genus_species, new_name) %>% 
   #manually cleaning names that were not recognized in World Flora
-  mutate(new_name=ifelse(old_name=='Lespedeza bicolor for. alba', 'Lespedeza bicolor', new_name),
-         Plant.Name.Index=ifelse(old_name %in% c('Acacia mangium x auriculiformis','Acacia tortillis spp. heteracantha','Lespedeza maximowiezzi var. tomentella'), 'TRUE', Plant.Name.Index))
+  mutate(new_name=ifelse(genus_species=='Lespedeza bicolor for. alba', 'Lespedeza bicolor', new_name),
+         Plant.Name.Index=ifelse(genus_species %in% c('Acacia mangium x auriculiformis','Acacia tortillis spp. heteracantha','Lespedeza maximowiezzi var. tomentella'), 'TRUE', Plant.Name.Index))
+
+clean.list.annotated <- full.list %>% 
+  left_join(clean.list)
 
 
 #### write data to file ####
 
-write.csv(clean.list, "legume_clean_names.csv")
+write.csv(clean.list.annotated, "legume_clean_names.csv", row.names=F)
 
